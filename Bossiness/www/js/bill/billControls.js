43e520ced7a9f55a96fa6e1cf1e7fbaaadde3744
jinlongxi <308561217@qq.com>
2017-08-01 14:43:26 +0800

    商家账单页面修改

--- Bossiness/www/js/bill/billControls.js
+++ Bossiness/www/js/bill/billControls.js
@@ -2,6 +2,15 @@ angular.module('bill.controllers', [])
 
   //B端账单
   .controller('billHomeCtrl', function ($scope,billService) {
+    //账单类型
+    $scope.billTypeList=[
+      {id:1,type:1,text:'总充值'},
+      {id:2,type:4,text:'本店收本店'},
+      {id:3,type:3,text:'本店收他店'},
+      {id:4,type:5,text:'他店收本店'},
+      {id:5,type:2,text:'本店已开卡'}
+    ];
+
     billService.getUserPaymentBybizService(1).success(function (data) {
       //$scope.yearAndMonthPaymentList=data.yearAndMonthPaymentList;
       $scope.paymentsList=data.paymentsList;
